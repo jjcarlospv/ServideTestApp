fb2d40a53b25a914fb1079bcc5b426f6d131b967
macmini15 <macmini15@macmini15.local>
2015-07-11 13:59:38 -0500

    Initial commit

---
+++ app/src/main/java/project/example/jean/servidetestapp/MyService.java
@@ -0,0 +1,20 @@
+package project.example.jean.servidetestapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by macmini15 on 7/11/15.
+ */
+public class MyService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
